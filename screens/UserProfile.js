5eaad0ddadabb4ede6190a204b572666629f00b8
Pacifique Kwizera <kwizerapacifique19@gmail.com>
2022-10-22 22:35:00 +0200

    userprofile

--- screens/UserProfile.js
+++ screens/UserProfile.js
@@ -1,8 +1,15 @@
-import { View, Text, Image, ImageBackground } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 import { globalStyles } from "../data/GlobalStyles";
 import { LinearGradient } from "expo-linear-gradient";
 import { colors } from "../data/Colours";
+import { Feather, Entypo, AntDesign, MaterialIcons } from "@expo/vector-icons";
 
 const bgImage = require("../assets/images/bg-user-2.jpg");
 
@@ -37,21 +44,129 @@ export default function UserProfile() {
                 justifyContent: "space-around",
               }}
             >
-              <Image
-                source={require("../assets/images/avatar.jpg")}
-                style={{ borderRadius: 50, height: 100, width: 100 }}
-                resizeMode="contain"
-              />
-              <Text
+              <View>
+                <Image
+                  source={require("../assets/images/avatar.jpg")}
+                  style={{ borderRadius: 50, height: 100, width: 100 }}
+                  resizeMode="contain"
+                />
+                <TouchableOpacity
+                  style={{
+                    position: "absolute",
+                    top: 70,
+                    left: 70,
+                    zIndex: 3,
+                  }}
+                >
+                  <Feather name="edit" size={20} color={colors.yellow} />
+                </TouchableOpacity>
+              </View>
+              <View>
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                    fontSize: 40,
+                    color: colors.white,
+                    textShadowColor: colors.black,
+                    textShadowRadius: 13,
+                  }}
+                >
+                  John Doe
+                </Text>
+                <Text
+                  style={{
+                    color: colors.white_a,
+                    fontStyle: "italic",
+                    fontSize: 13,
+                  }}
+                >
+                  Joined 12/04/2017
+                </Text>
+                <View
+                  style={{
+                    flexDirection: "row",
+                  }}
+                >
+                  <Entypo name="mail" size={18} color={colors.yellow} />
+                  <Text
+                    style={{
+                      color: colors.white_a,
+                      fontWeight: "400",
+                      marginLeft: 5,
+                    }}
+                  >
+                    doe@gmail.com
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                  }}
+                >
+                  <Entypo name="location-pin" size={18} color={colors.yellow} />
+                  <Text
+                    style={{
+                      color: colors.white_a,
+                      fontWeight: "400",
+                      marginLeft: 5,
+                    }}
+                  >
+                    Kigali,Rwanda
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-around",
+                marginTop: 20,
+              }}
+            >
+              <View
                 style={{
-                  fontWeight: "bold",
-                  fontSize: 40,
-                  color: colors.white,
-                  textShadowColor: colors.black,
-                  textShadowRadius: 13,
+                  flexDirection: "row",
+                  alignItems: "center",
                 }}
               >
-                John Doe
+                <AntDesign name="like1" size={35} color={colors.yellow} />
+                <Text style={{ color: colors.yellow,fontWeight:'bold',fontSize:20, marginLeft: 5 }}>
+                  Likes
+                </Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <MaterialIcons
+                  name="post-add"
+                  size={35}
+                  color={colors.yellow}
+                />
+                <Text style={{ color: colors.yellow,fontWeight:'bold',fontSize:20, marginLeft: 5 }}>
+                  Wishlist
+                </Text>
+              </View>
+            </View>
+            <View style={{ marginTop: 20, paddingHorizontal: 20 }}>
+              <Text
+                style={{
+                  color: colors.white,
+                  fontSize: 32,
+                  fontWeight: "bold",
+                }}
+              >
+                About me
+              </Text>
+              <Text
+                style={{ color: colors.white, fontSize: 20, fontWeight: "300" }}
+              >
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe
+                a, atque voluptates reiciendis impedit ea id cum tempore, omnis
+                similique sunt, blanditiis deleniti asperiores eaque! Aliquid
+                sapiente expedita autem ipsam?
               </Text>
             </View>
           </LinearGradient>
